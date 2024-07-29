3b1f1c434fd375f85babfbb864a2b10ca8c0c578
udK01 <dominik.muranyi@gmail.com>
2024-07-29 13:47:45 +0100

    Adjusted fade in effect for backdrop.

--- Portfolio/src/components/Hero.jsx
+++ Portfolio/src/components/Hero.jsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <section>
       <div className="flex flex-col justify-center items-center w-full h-screen">
-        <div className="absolute w-full h-full flex items-center justify-center mt-[120px] circular-reveal">
+        <div className="absolute w-full h-full flex items-center justify-center mt-[120px] opacity-0 circular-reveal">
           <img
             src="./squarebackground.png"
             className="w-[85%] h-[85%] absolute"
