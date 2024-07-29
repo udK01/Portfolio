c59e31a805b1352da5e3cce2152bd5d56835fa9c
udK01 <dominik.muranyi@gmail.com>
2024-07-29 16:46:36 +0100

    Added spline animation.

--- Portfolio/src/components/Hero.jsx
+++ Portfolio/src/components/Hero.jsx
@@ -2,11 +2,16 @@ import TextBar from "./TextBar";
 
 import "./utils/hero.css";
 
+import Spline from "@splinetool/react-spline";
+
 export default function Hero() {
   return (
     <section>
       <div className="flex flex-col justify-center items-center w-full h-screen">
-        <div className="absolute w-full h-full flex items-center justify-center mt-[120px] opacity-0 circular-reveal">
+        <div className="absolute z-10 w-full h-screen circular-reveal opacity-0 mt-10">
+          <Spline scene="https://prod.spline.design/Tr9fJ1rZOicDY-Y1/scene.splinecode" />
+        </div>
+        <div className="absolute w-full h-full flex items-center justify-center mt-[140px] opacity-0 circular-reveal">
           <img
             src="./squarebackground.png"
             className="w-[85%] h-[85%] absolute"
