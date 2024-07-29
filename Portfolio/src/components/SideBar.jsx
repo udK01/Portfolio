cc0d5b603485f0ee78fdcecbdf7911fbcd6247b2
udK01 <dominik.muranyi@gmail.com>
2024-07-29 11:12:13 +0100

    Completed Sidebar.

--- Portfolio/src/components/SideBar.jsx
+++ Portfolio/src/components/SideBar.jsx
@@ -1,19 +1,40 @@
-import { FaLinkedin, FaGithub, FaDiscord } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaFacebook } from "react-icons/fa";
 
 export default function SideBar() {
   const elements = [
-    <FaLinkedin className="size-[35px] w-full pr-2" />,
-    <FaGithub className="size-[35px] w-full pr-2" />,
-    <FaDiscord className="size-[35px] w-full pr-2" />,
+    {
+      icon: (
+        <FaLinkedin className="size-[35px] w-full pr-2 hover:cursor-pointer hover:text-primary_purple transition-colors duration-300" />
+      ),
+      link: "https://www.linkedin.com/in/dominik-muranyi-07931827b/",
+    },
+    {
+      icon: (
+        <FaGithub className="size-[35px] w-full pr-2 hover:cursor-pointer hover:text-primary_purple transition-colors duration-300" />
+      ),
+      link: "https://github.com/udK01",
+    },
+    {
+      icon: (
+        <FaFacebook className="size-[35px] w-full pr-2 hover:cursor-pointer hover:text-primary_purple transition-colors duration-300" />
+      ),
+      link: "https://www.facebook.com/dominik.muranyi.5/?locale=en_GB",
+    },
   ];
 
   return (
     <div className="absolute h-screen flex items-center">
       <div className="fixed w-[80px] h-[250px] bg-[#242020] space-y-[30px] flex flex-col justify-center rounded-r-[45px]">
         {elements.map((element, index) => (
-          <div key={index} className="text-white">
-            {element}
-          </div>
+          <a
+            key={index}
+            href={element.link}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="block text-white"
+          >
+            {element.icon}
+          </a>
         ))}
       </div>
     </div>
