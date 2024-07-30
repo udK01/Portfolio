2b844b3c13c53785de9c7d6ceb1973e368e28c25
udK01 <dominik.muranyi@gmail.com>
2024-07-30 20:27:52 +0100

    Completed Stack Element.

--- Portfolio/src/components/Stack/Stack.jsx
+++ Portfolio/src/components/Stack/Stack.jsx
@@ -3,12 +3,16 @@ import StackElement from "./StackElement";
 
 export default function Stack() {
   return (
-    <section className="space-y-[150px]">
+    <section className="space-y-[150px] overflow-hidden">
       <div className="w-full flex justify-center">
         <TextBar text={"STACK"} shadow={"top_right_green"} />
       </div>
-      <div className="w-full flex justify-center">
-        <StackElement icon={"./React.png"} />
+      <div className="w-full flex justify-center hover:cursor-pointer">
+        <StackElement icon={"./React.png"} text={"React"} colour={"purple"} />
+        <StackElement icon={"./Js.png"} text={"Javascript"} colour={"green"} />
+        <StackElement icon={"./Figma.png"} text={"Figma"} colour={"purple"} />
+        <StackElement icon={"./Html.png"} text={"HTML"} colour={"green"} />
+        <StackElement icon={"./Github.png"} text={"GitHub"} colour={"purple"} />
       </div>
     </section>
   );
--- Portfolio/src/components/Stack/StackElement.jsx
+++ Portfolio/src/components/Stack/StackElement.jsx
@@ -1,7 +1,7 @@
-function Icon({ icon, boxShadow }) {
+function Icon({ icon, boxShadow, borderColour }) {
   return (
     <div
-      className="w-[100px] h-[100px] rounded-full border-2 border-primary_green flex items-center justify-center"
+      className={`w-[100px] h-[100px] rounded-full border-2 ${borderColour} flex items-center justify-center`}
       style={{ boxShadow }}
     >
       <img className="size-[65px]" src={icon} alt="Icon" />
@@ -9,40 +9,47 @@ function Icon({ icon, boxShadow }) {
   );
 }
 
-function GreenIcon({ icon }) {
+function TextWithShadow({ text, textShadow, textColour }) {
   return (
-    <div className="flex gap-[8px]">
-      <Icon
-        icon={icon}
-        boxShadow="inset 4px 4px 4px #4CFFB4, 4px 4px 4px #9747FF"
-      />
-      <Text text={"React"} />
-    </div>
-  );
-}
-
-function PurpleIcon({ icon }) {
-  return (
-    <Icon
-      icon={icon}
-      boxShadow="inset -4px 4px 4px #9747FF, -4px 4px 4px #4CFFB4"
-    />
-  );
-}
-
-function Text({ text }) {
-  return (
-    <div className="w-fit font-poppins font-black text-[60px] text-primary_green">
+    <div
+      className={`w-fit font-poppins font-black text-[60px] ${textColour}`}
+      style={{ textShadow }}
+    >
       {text}
     </div>
   );
 }
 
-export default function StackElement({ icon, text }) {
+export default function StackElement({ icon, text, colour }) {
+  const isGreen = colour === "green";
+  const borderColour = isGreen
+    ? "border-primary_green"
+    : "border-primary_purple";
+  const iconBorderColour = isGreen
+    ? "border-primary_purple"
+    : "border-primary_green";
+  const textColour = isGreen ? "text-primary_purple" : "text-primary_green";
+  const boxShadow = isGreen
+    ? "inset 4px 4px 4px #4CFFB4, 4px 4px 4px #9747FF"
+    : "inset -4px 4px 4px #9747FF, -4px 4px 4px #4CFFB4";
+  const textShadow = isGreen ? "3px 3px #4CFFB4" : "3px 3px #9747FF";
+
   return (
-    <div className="w-[400px] h-[150px] border-2 border-primary_green">
-      <GreenIcon icon={icon} />
-      <p>{text}</p>
+    <div
+      className={`w-fit mx-[9px] px-[63px] h-[150px] border-2 ${borderColour} flex items-center justify-center bg-[#131010]`}
+    >
+      <div className="flex gap-[8px] items-center">
+        <Icon
+          icon={icon}
+          boxShadow={boxShadow}
+          borderColour={iconBorderColour}
+        />
+        <TextWithShadow
+          text={text}
+          textShadow={textShadow}
+          textColour={textColour}
+        />
+      </div>
     </div>
   );
 }
