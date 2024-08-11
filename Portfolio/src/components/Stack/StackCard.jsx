978fa6cc76218a4e7bef3238cde3b2555789677a
udK01 <dominik.muranyi@gmail.com>
2024-08-11 18:22:11 +0100

    Fixed hover bugs.

--- Portfolio/src/components/Stack/StackCard.jsx
+++ Portfolio/src/components/Stack/StackCard.jsx
@@ -10,11 +10,11 @@ export default function StackCard({ icon, text }) {
       onMouseLeave={() => setHover(false)}
     >
       <div
-        className="size-[120px] flex items-center justify-center border-2 transition-all ease-in-out duration-300 border-primary_purple hover:w-fit hover:px-6 hover:cursor-pointer hover:border-primary_green"
+        className="min-w-[128px] min-h-[120px] flex items-center justify-center border-2 transition-all  ease-in-out duration-300 border-primary_purple hover:cursor-pointer hover:border-primary_green hover:scale-[110%]"
         style={{ boxShadow: !hover ? "4px 4px #9747FF" : "4px 4px #4CFFB4" }}
       >
-        <img src={icon} className={`size-20 m-4 ${hover && "hidden"}`} />
-        <span className={`${!hover && "hidden"}`}>{text}</span>
+        <img src={icon} className={`size-[60px] m-4 ${hover && "hidden"}`} />
+        <span className={`${!hover && "hidden"} text-[20px]`}>{text}</span>
       </div>
     </div>
   );
