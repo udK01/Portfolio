cca4b36ad44c2b8fc9c847cecf01ea68de5914a8
udK01 <dominik.muranyi@gmail.com>
2024-08-11 18:03:34 +0100

    Added array for next stack elements.

--- Portfolio/src/components/Stack/StackCard.jsx
+++ Portfolio/src/components/Stack/StackCard.jsx
@@ -10,7 +10,7 @@ export default function StackCard({ icon, text }) {
       onMouseLeave={() => setHover(false)}
     >
       <div
-        className="size-[120px] flex items-center justify-center border-2 transition-all ease-in-out duration-300 border-primary_purple hover:w-fit hover:px-6 hover:cursor-pointer hover:border-primary_green hover:scale-[110%]"
+        className="size-[120px] flex items-center justify-center border-2 transition-all ease-in-out duration-300 border-primary_purple hover:w-fit hover:px-6 hover:cursor-pointer hover:border-primary_green"
         style={{ boxShadow: !hover ? "4px 4px #9747FF" : "4px 4px #4CFFB4" }}
       >
         <img src={icon} className={`size-20 m-4 ${hover && "hidden"}`} />
