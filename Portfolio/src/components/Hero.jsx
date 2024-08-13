41101db3dee2ea4abb250bf40d9b35746552ddcd
udK01 <dominik.muranyi@gmail.com>
2024-08-13 16:32:02 +0100

    Took out landing page.

--- Portfolio/src/components/Hero.jsx
+++ Portfolio/src/components/Hero.jsx
@@ -8,16 +8,25 @@ export default function Hero() {
   return (
     <section id="ABOUT ME">
       <div className="flex flex-col justify-center items-center w-full h-screen">
-        <div className="absolute z-10 w-full h-screen circular-reveal opacity-0 mt-10">
-          <Spline scene="https://prod.spline.design/Tr9fJ1rZOicDY-Y1/scene.splinecode" />
-        </div>
         <div className="absolute w-full h-full flex items-center justify-center mt-[140px] opacity-0 circular-reveal">
           <img
             src="./Backgrounds/squarebackground.png"
             className="w-[85%] h-[85%] absolute"
           />
         </div>
-        <TextBar
+      </div>
+    </section>
+  );
+}
+
+{
+  /* <div className="absolute z-10 w-full h-screen circular-reveal opacity-0 mt-10">
+          <Spline scene="https://prod.spline.design/Tr9fJ1rZOicDY-Y1/scene.splinecode" />
+        </div> */
+}
+
+{
+  /* <TextBar
           text={"DOMINIK"}
           shadow={"top_left_purple"}
           direction={"left"}
@@ -34,8 +43,5 @@ export default function Hero() {
           shadow={"bottom_left_purple"}
           direction={"left"}
           extra={"-translate-x-[50px] z-10 opacity-0"}
-        />
-      </div>
-    </section>
-  );
+        /> */
 }
