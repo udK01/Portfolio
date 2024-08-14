ff5cbfa738bff01076f6f5aa6928c2623e425353
udK01 <dominik.muranyi@gmail.com>
2024-08-14 20:29:33 +0100

    Added sub-title.

--- Portfolio/src/components/AboutMe.jsx
+++ Portfolio/src/components/AboutMe.jsx
@@ -3,6 +3,29 @@ import TextBar from "./TextBar";
 import "./utils/aboutme.css";
 
 export default function AboutMe() {
+  const front = [
+    { icon: "./Icons/React.png", text: "React" },
+    { icon: "./Icons/Tailwind.png", text: "Tailwind" },
+    { icon: "./Icons/Js.png", text: "JavaScript" },
+    { icon: "./Icons/Html.png", text: "HTML" },
+  ];
+
+  const back = [
+    { icon: "./Icons/Laravel.png", text: "Laravel" },
+    { icon: "./Icons/MySQL.png", text: "MySQL" },
+    { icon: "./Icons/Node.png", text: "Node" },
+    { icon: "./Icons/Php.png", text: "Php" },
+  ];
+
+  const other = [
+    { icon: "./Icons/Unity.png", text: "Unity" },
+    { icon: "./Icons/Trello.png", text: "Trello" },
+    { icon: "./Icons/Github.png", text: "GitHub" },
+    { icon: "./Icons/Figma.png", text: "Figma" },
+    { icon: "./Icons/Docker.png", text: "Docker" },
+    { icon: "./Icons/VsCode.png", text: "VsCode" },
+  ];
+
   return (
     <section className="h-screen">
       {/* Background */}
@@ -16,6 +39,9 @@ export default function AboutMe() {
       </div>
 
       {/* Sub-Title */}
+      <div className="font-robot font-black text-[60px] text-primary_green ml-[20%] z-50 scroll_animate">
+        Who am I?
+      </div>
 
       {/* Paragraphs */}
       <div className="w-[60%] mx-auto font-poppins text-white text-[30px]">
