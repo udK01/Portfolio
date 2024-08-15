76d578baf9de9284003e39f10c6ad30258847194
udK01 <dominik.muranyi@gmail.com>
2024-08-15 17:12:57 +0100

    Started working on contact section.

--- Portfolio/src/components/Contact.jsx
+++ Portfolio/src/components/Contact.jsx
@@ -1,8 +1,14 @@
 export default function Contact() {
   return (
-    <section
-      id="CONTACT"
-      className="h-screen flex flex-col justify-center"
-    ></section>
+    <section id="CONTACT" className="h-screen flex flex-col justify-center">
+      <div className="flex justify-center">
+        <div className="flex justify-center bg-[#242020] w-[30%]">
+          <input
+            placeholder="Enter Your Name"
+            className="border-2 border-transparent bg-transparent"
+          />
+        </div>
+      </div>
+    </section>
   );
 }
