d81a37c9faf0242b858032b7158604deda06b478
udK01 <dominik.muranyi@gmail.com>
2024-08-16 14:44:15 +0100

    Added dropdown for tablet and phone.

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useState } from "react";
+import { FiAlignJustify } from "react-icons/fi";
+
 import "./utils/navbar.css";
 
 export default function Navbar() {
   const menuItems = ["About Me", "Projects", "Contact"];
   const items = ["Hero", "About Me", "Projects", "Contact"];
   const [highlight, setHighlight] = useState(-1);
+  const [showMenu, setShowMenu] = useState(false);
 
   // useEffect(() => {
   //   const elements = items.map((item) =>
@@ -55,25 +58,56 @@ export default function Navbar() {
   };
 
   return (
-    <div className="flex justify-between fixed bg-primary_gray h-[117px] items-center w-full navbar-move z-40">
-      <img src="./logo.png" className="w-[108px] h-[64px] ml-[32px]" />
-      <div className="flex text-[#7E7E7E] font-medium gap-20 font-poppins text-[25px]">
-        {menuItems.map((item, index) => (
-          <p
-            key={index}
-            onClick={() => handleScroll(item)}
-            className={`hover:cursor-pointer hover:text-primary_green transition-all duration-300 ${
-              highlight === index ? "text-primary_green" : ""
-            }`}
-          >
-            {item}
-          </p>
-        ))}
+    <div className="w-full flex items-center bg-primary_gray h-[117px] navbar-move z-40">
+      {/* Laptop & Desktop */}
+      <div className="justify-between w-full items-center 2xs:hidden lg:flex">
+        <img src="./logo.png" className="w-[108px] h-[64px] ml-[32px]" />
+        <div className="flex text-[#7E7E7E] font-medium gap-20 font-poppins text-[25px]">
+          {menuItems.map((item, index) => (
+            <p
+              key={index}
+              onClick={() => handleScroll(item)}
+              className={`hover:cursor-pointer hover:text-primary_green transition-all duration-300 ${
+                highlight === index ? "text-primary_green" : ""
+              }`}
+            >
+              {item}
+            </p>
+          ))}
+        </div>
+        <div
+          className={`resume-border font-robot font-black hover:cursor-pointer text-[20px] hover:text-primary_green transition-colors duration-300 mr-[32px]`}
+        >
+          Resume.
+        </div>
       </div>
-      <div
-        className={`resume-border font-robot font-black hover:cursor-pointer text-[20px] hover:text-primary_green transition-colors duration-300 mr-[32px]`}
-      >
-        Resume.
+      {/* Phone & Tablet */}
+      <div className="justify-between w-full items-center 2xs:flex lg:hidden">
+        <img src="./logo.png" className="w-[108px] h-[64px] mx-[32px]" />
+        <div className="flex flex-col">
+          <FiAlignJustify
+            size={48}
+            color="white"
+            className="mx-[32px]"
+            onClick={() => setShowMenu(!showMenu)}
+          />
+          {showMenu && (
+            <div className="absolute flex flex-col font-poppins p-5 text-[20px] space-y-10 text-white items-center right-0 z-20 bg-primary_gray">
+              {menuItems.map((item, index) => (
+                <p
+                  key={index}
+                  onClick={() => handleScroll(item)}
+                  className={`hover:cursor-pointer hover:text-primary_green transition-all duration-300 ${
+                    highlight === index ? "text-primary_green" : ""
+                  }`}
+                >
+                  {item}
+                </p>
+              ))}
+              <p>Resume.</p>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
