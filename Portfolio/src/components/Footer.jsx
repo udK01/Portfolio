6e2e4308b6aaab04a31e8b1ddb56c58bab61418a
udK01 <dominik.muranyi@gmail.com>
2024-08-21 16:02:16 +0100

    Adjusted email in footer.

--- Portfolio/src/components/Footer.jsx
+++ Portfolio/src/components/Footer.jsx
@@ -10,7 +10,7 @@ export default function Footer() {
     <div className="flex justify-center items-center h-[117px] mt-10 w-full">
       {/* Icons */}
       <div className="flex gap-[50px] z-10">
-        <CiMail className={style} title="dominik.muranyi@gmail.com" />
+        <CiMail className={style} title="dominik.muranyi.dev@gmail.com" />
         <FaDiscord className={style} title="udKay" />
         <a href="https://www.linkedin.com/in/dominik-muranyi-07931827b/">
           <FaLinkedin className={style} title="Dominik Muranyi" />
