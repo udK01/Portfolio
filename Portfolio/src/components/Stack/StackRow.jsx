d51359f30cdbc4fb641c54ec188fb82c8aa7935c
udK01 <dominik.muranyi@gmail.com>
2024-08-21 11:58:21 +0100

    Adjusted stack row spacing on mobile.

--- Portfolio/src/components/Stack/StackRow.jsx
+++ Portfolio/src/components/Stack/StackRow.jsx
@@ -6,7 +6,7 @@ export default function StackRow({ text, stack }) {
       <div className="font-robot font-semibold lg:text-[30px] md:text-[20px] text-primary_green">
         {text}
       </div>
-      <div className="flex xl:gap-10 md:gap-6">
+      <div className="flex flex-wrap justify-center 2xs:w-[80%] md:w-full xl:gap-10 md:gap-6 2xs:gap-4">
         {stack.map((el, index) => (
           <StackCard key={index} icon={el.icon} text={el.text} />
         ))}
