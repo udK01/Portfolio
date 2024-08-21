c4c4d0e6bb91d84d9802681af3e0106815de4197
udK01 <dominik.muranyi@gmail.com>
2024-08-21 12:15:53 +0100

    Adjusted project hover effect scaling.

--- Portfolio/src/components/Projects/ProjectCard.jsx
+++ Portfolio/src/components/Projects/ProjectCard.jsx
@@ -1,6 +1,6 @@
 export default function ProjectCard({ src }) {
   return (
-    <div className="xl:w-[200px] xl:h-[300px] lg:w-[150px] lg:h-[250px] md:w-[200px] md:h-[300px] 2xs:w-[150px] 2xs:h-[250px]  filter grayscale transition-all duration-500 ease-in-out hover:w-[400px] hover:grayscale-0 hover:filter-none hover:cursor-pointer">
+    <div className="xl:w-[200px] xl:h-[300px] lg:w-[150px] lg:h-[250px] md:w-[200px] md:h-[300px] 2xs:w-[150px] 2xs:h-[250px]  filter grayscale transition-all duration-500 ease-in-out xl:hover:w-[400px] lg:hover:w-[250px] md:hover:w-[300px] 2xs:hover:w-[250px] hover:grayscale-0 hover:filter-none hover:cursor-pointer">
       <img
         src={src}
         className="relative w-full h-full object-cover border-2 border-primary_purple"
